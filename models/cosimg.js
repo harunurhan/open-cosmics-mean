f3d3461ace931a0d99e4299c9174a811a0da692e
harunurhan <harunurhan17@gmail.com>
2015-08-01 12:53:33 +0200

    change Model

--- models/cosimg.js
+++ models/cosimg.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var cosimgSchema = new Schema({
-	imgPath: String,
-	filePath: String,
+	paths: [{imgPath: String, filePath: String}],
 	longitude: Number,
 	latitude: Number
 });
